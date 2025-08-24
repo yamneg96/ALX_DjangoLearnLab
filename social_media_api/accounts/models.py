55cd2a1cfe7838310be24d17db9c1cdef05a1b0b
Yamlak Negash <yamlaknegash96@gmail.com>
2025-08-25 00:04:35 +0300

    Setup & Authentication for Social Media API

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,3 +1,21 @@
+# accounts/models.py
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+def user_upload_path(instance, filename):
+    return f"profile_pics/{instance.username}/{filename}"
+
+class User(AbstractUser):
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to=user_upload_path, blank=True, null=True)
+    # One M2M to self with asymmetric follows:
+    # "followers" gives you reverse "following" automatically
+    followers = models.ManyToManyField(
+        "self",
+        symmetrical=False,
+        related_name="following",
+        blank=True
+    )
+
+    def __str__(self):
+        return self.username
