a06c6d2d6b8d47d57a9f690b0a1f7ace62f57893
Yamlak Negash <yamlaknegash96@gmail.com>
2025-08-25 00:27:44 +0300

    Fixed some issues on posts

--- social_media_api/posts/serializers.py
+++ social_media_api/posts/serializers.py
@@ -1,20 +1,18 @@
-# posts/serializers.py
 from rest_framework import serializers
 from .models import Post, Comment
 
-class CommentSerializer(serializers.ModelSerializer):
-    author_username = serializers.ReadOnlyField(source='author.username')
-
-    class Meta:
-        model = Comment
-        fields = ["id", "post", "author", "author_username", "content", "created_at", "updated_at"]
-        read_only_fields = ["author"]
-
 class PostSerializer(serializers.ModelSerializer):
-    author_username = serializers.ReadOnlyField(source='author.username')
-    comments = CommentSerializer(many=True, read_only=True)
+    author = serializers.ReadOnlyField(source='author.username')  # show username instead of ID
 
     class Meta:
         model = Post
-        fields = ["id", "author", "author_username", "title", "content", "created_at", "updated_at", "comments"]
-        read_only_fields = ["author"]
+        fields = ['id', 'title', 'content', 'author', 'created_at', 'updated_at']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+    post = serializers.PrimaryKeyRelatedField(queryset=Post.objects.all())
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'content', 'author', 'created_at', 'updated_at']
